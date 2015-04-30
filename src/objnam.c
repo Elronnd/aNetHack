c6b123f949a8b28893802369ff27b6da4e50134a
Elronnd <elijah@shalott.net>
2015-04-30 13:22:31 -0700

    minor bug

--- src/objnam.c
+++ src/objnam.c
@@ -634,6 +634,7 @@ boolean with_price;
 			&& obj->otyp != SCR_MAIL
 #endif
 			&& obj->otyp != FAKE_AMULET_OF_YENDOR
+			&& obj->otyp != EXPENSIVE_PLATINUM_SPELLBOUND_
 			&& obj->otyp != AMULET_OF_YENDOR
 			&& !Role_if(PM_PRIEST))
 		Strcat(prefix, "uncursed ");
